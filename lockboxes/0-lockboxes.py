51aad8080d345ab4128a14e89f8b952a9f2d2d39
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-12-05 11:41:12 +0100

    refactor: enhance canUnlockAll function with detailed logic for unlocking boxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
+"""Utilities for checking if a set of lockboxes can all be opened."""
+
 
 def canUnlockAll(boxes):
     """Check if all boxes can be opened"""
+
+    unlocked = [False] * len(boxes)
+    unlocked[0] = True
+
+    keys = [0]
+
+    while keys:
+        box = keys.pop()
+        for key in boxes[box]:
+            if key < len(boxes) and not unlocked[key]:
+                unlocked[key] = True
+                keys.append(key)
+
+    return all(unlocked)
