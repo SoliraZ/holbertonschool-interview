2c199baa367fa0ad64a0246c01272a21db2e96e9
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2026-07-09 15:16:44 +0200

    add: implement print_array function to display elements of an integer array

---
+++ merge_sort/print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * print_array - prints an array of integers
+ * @array: array of integers
+ * @size: number of elements
+ */
+void print_array(const int *array, size_t size)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+}
